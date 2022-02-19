19043046b91b26ac8d47599713c105ad24a63b47
ShinchanShukla144 <77481704+ShinchanShukla144@users.noreply.github.com>
2022-02-19 23:36:54 +0530

    Add files via upload

---
+++ MainFile.js
@@ -0,0 +1,15 @@
+setTimeout(()=>{
+    document.getElementById('full').style.display = "none";
+},4000)
+
+setTimeout(()=>{
+    document.getElementById('spn').style.display = "inline-block";
+    document.getElementById("spn1").style.display = "none"
+
+},2000)
+// document.getElementById("afterMotion").style.display = "none"
+
+
+setTimeout(()=>{
+    document.getElementById("afterMotion").style.display = "block"
+},4000)
